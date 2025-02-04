e641421e28c40dd0b84f3582118b6bce5efef9a4
l3xlL <barbarechikobava7@gmail.com>
2025-02-04 19:48:52 +0400

    day 1

---
+++ day 1/homework/lesson01.py
@@ -0,0 +1,32 @@
+#stringebi
+name = "barbare"
+surname = "chikobava"
+
+# print(name)
+
+#integar / int
+age = 15
+
+#float / atwiladi
+height = 172.3
+
+
+#true an false
+knows_programming =True
+is_ugly = False
+
+#js uppercase
+isUgly = False
+
+print(name + " " + surname)
+
+
+
+# stringi iwereba brwyalebshi
+# print(name + age)
+
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programming))
