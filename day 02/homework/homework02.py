973d91b4c7461e548899dbc36ded4dc854d832c9
l3xlL <barbarechikobava7@gmail.com>
2025-02-13 22:50:38 +0400

    homework day 2

---
+++ day 02/homework/homework02.py
@@ -0,0 +1,36 @@
+# davaleba 1
+
+first_string = "hello world"
+
+first_int = 24
+
+print("String:", first_string)
+print("Integer:", first_int)
+
+
+
+# davaleba 2
+
+second_string = "gamarjoba"
+second_string_gameoreba = "elementi " * 10
+
+
+pirveli = 1
+meore = 999
+sheerteba = int(pirveli + meore)
+
+
+gamarjoba = "hello "
+msoflio = "world"
+gamarjoba_msoflio = gamarjoba + msoflio
+
+boolean_value = 5 * 2 == 10
+
+
+
+
+print(second_string)
+print(second_string_gameoreba)
+print(sheerteba)
+print(gamarjoba_msoflio)
+print(boolean_value)
