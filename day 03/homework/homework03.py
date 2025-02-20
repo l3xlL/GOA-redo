0ce21b23715c7415554484652a2b39a1ccf02a03
l3xlL <barbarechikobava7@gmail.com>
2025-02-20 19:47:19 +0400

    hw 3 fix

--- day 03/homework/homework03.py
+++ day 03/homework/homework03.py
@@ -1,26 +1,28 @@
 # task 1
 
-num1 = float(input("chawere pirveli ricxvi: "))
-num2 = float(input("chawere meore ricxvi: "))
-num3 = float(input("chawere mesame ricxvi: "))
-num4 = float(input("chawere meotxe ricxvi: "))
+# num1 = float(input("chawere pirveli ricxvi: "))
+# num2 = float(input("chawere meore ricxvi: "))
+# num3 = float(input("chawere mesame ricxvi: "))
+# num4 = float(input("chawere meotxe ricxvi: "))
 
-jami = (num1 + num2 + num3 + num4) / 4
+# jami = (num1 + num2 + num3 + num4) / 4
 
-print("am ricxvebis sashualo aritmetikuli aris: " + str(jami))
+# print("am ricxvebis sashualo aritmetikuli aris: " + str(jami))
 
 # task 2
 
-saxeli = input("Enter your first name: ")
-gvari = input("Enter your last name: ")
+# saxeli = input("chawere sheni saxeli: ")
+# gvari = input("chawere sheni gvari: ")
 
-print("mogesalmebit " + saxeli + " " + gvari)
+# print("mogesalmebit " + saxeli + " " + gvari)
 
 # task 3
 
 num5 = float(input("chawere pirveli ricxvi: "))
 num6 = float(input("chawere meore ricxvi: "))
 
-shedegi = num5 + "😊" + num6
+emoji = chr(128522)
+
+shedegi = str(num5) + emoji + str(num6)
 
 print("shedegi: " + shedegi)
