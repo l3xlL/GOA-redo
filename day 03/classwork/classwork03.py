09b87996b38c1c94af69142b29cdd259c26b28b7
l3xlL <barbarechikobava7@gmail.com>
2025-02-24 16:16:07 +0400

    day 04 homework

--- day 03/classwork/classwork03.py
+++ day 03/classwork/classwork03.py
@@ -1,1 +1,47 @@
-# no classwork
+# task 1
+
+num1 = float(input("chawere pirveli ricxvi: "))
+num2 = float(input("chawere meore ricxvi: "))
+num3 = float(input("chawere mesame ricxvi: "))
+
+jami = num1 + num2 + num3
+
+print("ricxvebis jami:", jami)
+
+
+# task 2
+
+user_1 = int(input("chawere saxeli: "))
+
+print(user_1)
+
+# task 3
+
+
+numb1 = float(input("chawere sheni sasurveli ricxvi: "))
+numb2 = float(input("chawere sheni meore sasurveli ricxvi: "))
+
+if numb1 > numb2:
+    print(True)
+else:
+    print(False)
+
+
+
+# task 4
+
+sityva = str(input("chawere sityva: "))
+n = int(input("chawere ramdenjer ganmeordeba es sityva: "))
+
+print(sityva * n)
+
+# task 5
+
+number1 = input("chawere pirveli ricxvi: ")
+number2 = input("chawere meore ricxvi: ")
+
+jami1 = number1 + number2
+
+result = str(int(jami1) + 1)
+
+print(result)
---
+++ day 04/homework/homework04.py
@@ -0,0 +1,17 @@
+# task 1
+
+# age = int(input("chawere nebismieri asaki: "))
+
+# if age > 2:
+#     print("shen bagshi unda iaro")
+# elif age >= 6:
+#     print("shen skolashi unda iaro")
+# else:
+#     print("shen samsaxurshi unda iaro")
+
+# task 2
+
+akademia = input("romel akademiashi dadixar?: ")
+
+if akademia == "Goa":
+    print("chad")
