83187a4bef99608d31c0446a3bc5d36fb589c703
l3xlL <barbarechikobava7@gmail.com>
2025-03-02 23:01:23 +0400

    classwork 07

--- day 07/classwork/classwork07.py
+++ day 07/classwork/classwork07.py
@@ -1,1 +1,43 @@
-# no classwork yet
+# task 1
+
+# text = str(input("chawere texti: "))
+# num = int(input("chawere ricxvi: "))
+
+
+# print(text[num])
+
+# task 2
+
+# for i in range(100, 0, -1):
+#     print(i)
+
+# task 3
+
+# for i in range(100, 0, -1):
+#     if i % 2 != 0:
+#         print(i)
+
+# task 4
+
+# for i in range(250, 500):
+#     if i % 10 == 0:
+#         print(i)
+
+# task 5
+
+# texti = input("chawere texti: ")
+
+# if texti.startswith('_'):
+#     print(True)
+# else:
+#     print(False)
+
+# შექმენით პროგრამა რომელიც მომხმარებელს ეკითხება რიცხვს და თუ ეს ტიცხვი ლუწია 10-ჯერ გამოუტანს "yes", ხოლო თუ ის კენტია "No'-ს
+
+num4 = int(input("chawere ricxvi: "))
+
+if num4 % 2 == 0:
+    for i in range(10):
+       print("yes")
+else:
+    print("no")
