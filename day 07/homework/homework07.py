bd171dca4380d64ebc06a5914caa5f02ff93afc3
l3xlL <barbarechikobava7@gmail.com>
2025-03-03 17:34:59 +0400

    homework 07

--- day 07/homework/homework07.py
+++ day 07/homework/homework07.py
@@ -1,1 +1,17 @@
-# no hw yet
+# task 1
+
+num = int(input("chawere ricxvi: "))
+
+print("am ricxvis yvela gamyofi aris:")
+for i in range(1, num + 1):
+    if num % i == 0:
+        print(i)
+
+# task 2
+
+#  ახსენით რა განსხვავებაა While და for loop-ებს შორის
+
+# for gamoiyeneba, roca mititebuli gaq tu ramdenjer unda gameordes ragac da while gamoiyeneba roca funqciss gagrdzeleba ginda manam sanam ragac False - it ar dagibrundeba
+
+# task 4 რა არის ამ გამოსახულების პასუხი:
+(1 and 0) or (1 and 1) and (0 or 0)
