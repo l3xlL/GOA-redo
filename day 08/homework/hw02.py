4947aa4a885027590816f29ae910715e1b64850a
l3xlL <barbarechikobava7@gmail.com>
2025-03-09 23:15:49 +0400

    day 08 hw02 fix

---
+++ day 08/homework/hw02.py
@@ -0,0 +1,1 @@
+# გაიარეთ Python introduction "Memory & Variables"-ის ჩათვლით
--- day 08/homework/hw05.py
+++ day 08/homework/hw05.py
@@ -1,25 +1,10 @@
-import random
-
-num1 = random.randint(1, 7)
-num2 = num1 + random.randint(1, 5)
-num3 = num2 + (num2 - num1)
-num4 = num3 + (num2 - num1)
-num5 = num4 + (num2 - num1)
-
-tanmimdevroba = [num1, num2, num3, num4, num5]
-
-hidden_index = random.randint(0, 4)
-hidden_num = tanmimdevroba[hidden_index]
-tanmimdevroba[hidden_index] = 0
-
 print("gamoicani ricxvebis tanmimdevroba")
-print(tanmimdevroba)
 
 while True:
     user = int(input("sheiyvane ricxvi: "))
-
-    if user == hidden_num:
-        print("gamoicani")
+    
+    if user == 5990:
+        print("gamoicani!")
         break
     else:
         print("ar gamoicani")
