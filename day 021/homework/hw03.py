d193102eb1077e6c3e541f78a7a174de4fea1c63
l3xlL <barbarechikobava7@gmail.com>
2025-04-28 21:03:37 +0400

    :p

---
+++ day 021/homework/hw03.py
@@ -0,0 +1,7 @@
+def cookie(x):
+    if type(x) == str:
+        return "Who ate the last cookie? It was Zach!"
+    elif type(x) == int or type(x) == float:
+        return "Who ate the last cookie? It was Monica!"
+    else:
+        return "Who ate the last cookie? It was the dog!"
