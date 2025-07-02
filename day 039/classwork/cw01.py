091d5fb84b8eefd2b538d6df2df1c19768bf0802
Aandria777 <korganashviliandria7@gmail.com>
2025-07-02 22:18:23 +0400

    !

---
+++ day 039/classwork/cw01.py
@@ -0,0 +1,7 @@
+def sum(numbers):
+    i = 0
+    total = 0
+    while i < len(numbers):
+        total += numbers[i]
+        i += 1
+    print(total)
---
+++ day 039/classwork/cw02.py
@@ -0,0 +1,7 @@
+def st(string_list):
+    i = 0
+    result = ""
+    while i < len(string_list):
+        result += string_list[i]
+        i += 1
+    print(result)
