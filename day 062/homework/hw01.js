ed295e666fc8883d53a72757c74067634b6e7ea9
l3xlL <barbarechikobava7@gmail.com>
2025-09-20 16:50:45 +0400

    chuu

--- day 062/homework/hw01.js
+++ day 062/homework/hw01.js
@@ -1,1 +1,3 @@
-// a
+// javascriptshi cvladis shesaqmnelad jer vwert tu rogori tipis cvladi gvinda rom shevqmnat.
+// aris 3 tipis: var, let da const
+// jer iwereba cvladis tipi shemdeg saxeli da mnishvneloba
